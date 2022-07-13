5ba5a09e86837fa8b8f32fba9558d3902e8a84d7
Younixue3 <bussiness.needs.rctka@gmail.com>
2022-07-13 14:16:34 +0800

    first commit

---
+++ src/views/Front/Auth/Signin.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+
+import carousel from 'vue-owl-carousel'
+
+export default {
+  name: "Signin",
+  components: {
+    carousel
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
