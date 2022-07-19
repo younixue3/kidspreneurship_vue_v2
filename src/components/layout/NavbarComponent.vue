baf094013e8e5b4b1af1dc5908b084b29471bd33
Younixue3 <bussiness.needs.rctka@gmail.com>
2022-07-19 13:29:23 +0800

    Fixing header

--- src/components/layout/NavbarComponent.vue
+++ src/components/layout/NavbarComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="px-5 py-3 bg-gray-800">
+  <div class="px-5 py-8 bg-gray-800 backdrop-blur-sm opacity-90">
     <div class="flex justify-between">
       <div class="w-96 text-yellow-400 font-bold text-center">Kidspreneurship</div>
       <div class="grid grid-cols-4 w-96 text-white mr-auto">
@@ -9,11 +9,11 @@
         <div>Publikasi</div>
       </div>
       <div class="w-52">
-        <div class="grid grid-cols-2 gap-5">
-          <button class="bg-transparent border-white border-2 text-white">
+        <div class="grid grid-cols-2 gap-3">
+          <button class="bg-transparent border-white border-2 text-white rounded-md">
             Sign In
           </button>
-          <button class="bg-white border-2 text-black">
+          <button class="bg-white border-2 text-black rounded-md">
             Sign Up
           </button>
         </div>
