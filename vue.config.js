843f28646aa8bf79c7caa16a51569ced3c20d315
Younixue3 <bussiness.needs.rctka@gmail.com>
2023-02-15 11:26:57 +0800

    fixing

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production' ? '/dist/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
   transpileDependencies: true,
 })
