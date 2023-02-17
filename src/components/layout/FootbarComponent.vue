91b5aa33ee122f9c5d05afd8f7db161950228b8c
Younixue3 <bussiness.needs.rctka@gmail.com>
2023-02-17 13:45:11 +0800

    Fixing

--- src/components/layout/FootbarComponent.vue
+++ src/components/layout/FootbarComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-amber-400 p-10 lg:p-32 px-12 lg:px-48 lg:flex justify-between">
+  <div class="bg-amber-400 p-10 lg:p-32 px-12 lg:px-32 lg:flex justify-between">
     <div>
       <img class="h-12 lg:h-24 mb-5 lg:mb-10" src="@/assets/hbics.png">
       <div class="text-left text-xs font-bold">Indrakila Street No. 99 G - Balikpapan 76125 East Borneo - Indonesia</div>
@@ -8,16 +8,16 @@
     <div class="mt-10 lg:mt-0">
       <h4 class="text-left text-xl lg:text-2xl font-bold uppercase mb-4">Related Links</h4>
       <ol class="text-left text-sm lg:text-base px-4">
-        <li><a href="http://hbics.sch.id/">Harapan Bangsa Integreted Christian School</a></li>
+        <li><a href="http://hbics.sch.id/">Harapan Bangsa Integrated Christian School</a></li>
       </ol>
     </div>
     <div class="mt-5 lg:mt-0">
       <h4 class="text-left text-xl lg:text-2xl font-bold uppercase mb-4">Social Media</h4>
       <ol class="list-none text-left text-sm lg:text-base px-4">
-        <li class="list-none"><FontAwesomeIcon class="w-5 mr-0.5" icon="fa-brands fa-facebook"/><a href="https://web.facebook.com/HBICS">Facebook</a></li>
-        <li class="list-none"><FontAwesomeIcon class="w-5 mr-0.5" icon="fa-brands fa-instagram"/><a href="https://www.instagram.com/harapanbangsa_balikpapan/">Instagram Harapan Bangsa Balikpapan</a></li>
-        <li class="list-none"><FontAwesomeIcon class="w-5 mr-0.5" icon="fa-brands fa-instagram"/><a href="https://www.instagram.com/harapanbangsa_balikpapan/">Instagram Kidspreneurship</a></li>
-        <li class="list-none"><FontAwesomeIcon class="w-5 mr-0.5" icon="fa-brands fa-youtube"/><a href="https://www.youtube.com/c/HappyHolyKidsHBICSBalikpapan">Youtube</a></li>
+        <li class="list-none"><FontAwesomeIcon class="w-5 mr-0.5" icon="fa-brands fa-facebook"/><a href="https://web.facebook.com/HBICS">Harapan Bangsa Integrated Christian School</a></li>
+        <li class="list-none"><FontAwesomeIcon class="w-5 mr-0.5" icon="fa-brands fa-instagram"/><a href="https://www.instagram.com/harapanbangsa_balikpapan/">Harapan Bangsa Integrated Christian School</a></li>
+        <li class="list-none"><FontAwesomeIcon class="w-5 mr-0.5" icon="fa-brands fa-instagram"/><a href="https://www.instagram.com/harapanbangsa_balikpapan/">Kidspreneurship</a></li>
+        <li class="list-none"><FontAwesomeIcon class="w-5 mr-0.5" icon="fa-brands fa-youtube"/><a href="https://www.youtube.com/c/HappyHolyKidsHBICSBalikpapan">Harapan Bangsa Integrated Christian School</a></li>
         <li class="list-none"><FontAwesomeIcon class="w-5 mr-0.5" icon="fa-brands fa-linkedin"/><a href="https://www.linkedin.com/company/ytcb/mycompany/">LinkedIn</a></li>
       </ol>
     </div>
