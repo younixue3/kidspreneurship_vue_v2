3b6ff0bf906239f60e4dbc2f5702318fa6b81a08
Younixue3 <bussiness.needs.rctka@gmail.com>
2023-03-07 09:56:00 +0800

    Fixng

--- src/views/Front/Event/EventDetailPage.vue
+++ src/views/Front/Event/EventDetailPage.vue
@@ -47,32 +47,38 @@ export default {
         {
           img: 'https://raw.githubusercontent.com/younixue3/stock_photo_rdev/main/andras-vas-Bd7gNnWJBkU-unsplash.webp',
           title: 'Aplikasi & Game',
-          file: process.env.VUE_APP_BASE_URL + 'event/logo/adg.docx',
+          file: process.env.VUE_APP_BASE_URL + 'event/logo/adg.pdf',
           desc: 'Kompetisi dalam pembangunan Aplikasi dan Game'
         },
         {
           img: 'https://raw.githubusercontent.com/younixue3/stock_photo_rdev/main/domenico-loia-hGV2TfOh0ns-unsplash.webp',
           title: 'Desain Grafis',
-          file: process.env.VUE_APP_BASE_URL + 'event/logo/dg.docx',
+          file: process.env.VUE_APP_BASE_URL + 'event/logo/dg.pdf',
           desc: 'Kompetisi di bidang Design Grafis'
         },
         {
           img: 'https://img.freepik.com/free-vector/set-sketches-beautiful-diverse-female-fashion-outfits_1150-50902.jpg?t=st=1677639831~exp=1677640431~hmac=5100d60fb7448f81ea08bafd3ffde510e591bb85ed775e4f201c824dea9ea43c',
           title: 'Fashion',
-          file: process.env.VUE_APP_BASE_URL + 'event/logo/fash.docx',
+          file: process.env.VUE_APP_BASE_URL + 'event/logo/fash.pdf',
           desc: 'Kompetisi dalam bidang Fashion Design'
         },
         {
           img: 'https://img.freepik.com/free-photo/front-view-breakfast-table-with-eggs-buns-cheese-fresh-juice-restaurant-during-daytime-food-meal-breakfast_140725-25904.jpg?t=st=1677639902~exp=1677640502~hmac=7a5900d4414fe0968cee84f98edf0eff8c5af5aeececcf81ab6adb36ee34403b',
           title: 'Food & Beverage',
-          file: process.env.VUE_APP_BASE_URL + 'event/logo/fdb.docx',
+          file: process.env.VUE_APP_BASE_URL + 'event/logo/fdb.pdf',
           desc: 'Kompetisi Food and Beverage'
         },
         {
           img: 'https://img.freepik.com/free-photo/giving-final-touches-artwork_1098-18131.jpg?t=st=1677640057~exp=1677640657~hmac=e34a24939bb35bda7da3410ad1379db22efaf73da37ae01a551f86276c9d985b',
           title: 'Kriya',
-          file: process.env.VUE_APP_BASE_URL + 'event/logo/kriy.docx',
+          file: process.env.VUE_APP_BASE_URL + 'event/logo/kriy.pdf',
           desc: 'Kompetisi Kriya'
+        },
+        {
+          img: 'https://img.freepik.com/free-photo/creativity-creative-ideas-imagination-inspiration-design-concept_53876-123736.jpg?w=1060&t=st=1678154053~exp=1678154653~hmac=27fa098ac139bb638b586e7df24f81303992452170d5f5a248ad08f7b62b962d',
+          title: 'Creative Product',
+          file: process.env.VUE_APP_BASE_URL + 'event/logo/sdcp.pdf',
+          desc: 'Kompetisi dalam membangun Creative Product'
         }
       ],
     }
