d4f3c1f07d65b471d8d6a780cf02c904c856808a
Younixue3 <bussiness.needs.rctka@gmail.com>
2023-03-16 04:38:59 +0800

    fixing

--- src/components/Back/Widget/FormSide.vue
+++ src/components/Back/Widget/FormSide.vue
@@ -206,14 +206,12 @@ export default {
               }
             })
             .then(resp => {
-
             })
 
       } else if (this.$store.state.formside.methode === 'put') {
         if (this.$store.state.formside.from == 'galeri') {
           this.$store.state.formside.url = this.$store.state.formside.url + 'updateGaleri/'
         }
-        console.log(process.env.VUE_APP_BASE_URL + this.$store.state.formside.url)
         axios.put(process.env.VUE_APP_BASE_URL + this.$store.state.formside.url,
             formData, {
               headers: {
@@ -221,11 +219,10 @@ export default {
               }
             })
             .then(resp => {
-              console.log(resp)
-              console.log('kontol')
             })
-      }
 
+      }
+      this.$store.commit('removeFormSide')
       // this.$router.go()
     },
     remove: function () {
