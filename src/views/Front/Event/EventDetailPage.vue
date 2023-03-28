f84f14be74eb4f0b0d785713bed744d1d7930ec7
Younixue3 <bussiness.needs.rctka@gmail.com>
2023-03-28 17:16:26 +0800

    Fixing

--- src/views/Front/Event/EventDetailPage.vue
+++ src/views/Front/Event/EventDetailPage.vue
@@ -79,7 +79,49 @@ export default {
           title: 'Creative Product',
           file: process.env.VUE_APP_BASE_URL + 'event/logo/sdcp.pdf',
           desc: 'Kompetisi dalam membangun Creative Product'
-        }
+        },
+        {
+          img: 'https://img.freepik.com/free-vector/set-sketches-beautiful-diverse-female-fashion-outfits_1150-50902.jpg?t=st=1677639831~exp=1677640431~hmac=5100d60fb7448f81ea08bafd3ffde510e591bb85ed775e4f201c824dea9ea43c',
+          title: 'Lomba Pendukung : Fashion Show',
+          file: process.env.VUE_APP_BASE_URL + 'event/logo/fs.pdf',
+          desc: 'Kompetisi dalam bidang Fashion Show'
+        },
+        {
+          img: 'https://img.freepik.com/free-photo/front-view-breakfast-table-with-eggs-buns-cheese-fresh-juice-restaurant-during-daytime-food-meal-breakfast_140725-25904.jpg?t=st=1677639902~exp=1677640502~hmac=7a5900d4414fe0968cee84f98edf0eff8c5af5aeececcf81ab6adb36ee34403b',
+          title: 'Lomba Pendukung : Food Presentation',
+          file: process.env.VUE_APP_BASE_URL + 'event/logo/fp.pdf',
+          desc: 'Kompetisi Food Presentation oleh peserta Lomba Pendukung'
+        },
+        {
+          img: 'https://img.freepik.com/free-photo/painting-dark-skinned-primary-school-age-boy-drawing-with-blue-pencil-sideways-camera-while-sitting-table-room-daylight_259150-59521.jpg?w=1060&t=st=1679993347~exp=1679993947~hmac=912c8d792381a178667c595025ae2c679deb3b2cc0ffa24397fc28bd51b62de1',
+          title: 'Lomba Pendukung : Coloring',
+          file: process.env.VUE_APP_BASE_URL + 'event/logo/cr.pdf',
+          desc: 'Lomba mewarnai oleh peserta Lomba Pendukung'
+        },
+        {
+          img: 'https://img.freepik.com/free-photo/three-kids-reading_23-2147782154.jpg?w=1060&t=st=1679993484~exp=1679994084~hmac=9f99a2c32715594c6716834f0dd126de85f9db2d6f752db45d13699d8f1e3703',
+          title: 'Lomba Pendukung : Story Telling',
+          file: process.env.VUE_APP_BASE_URL + 'event/logo/cr.pdf',
+          desc: 'Lomba Storytelling oleh peserta Lomba Pendukung'
+        },
+        {
+          img: 'https://img.freepik.com/free-photo/brother-sister-girl-boy-kid-joy-playful-leisure-concept_53876-71147.jpg?w=740&t=st=1679994375~exp=1679994975~hmac=25f48441d8c9c7ad6b608409ca9dc0e1d84c7473f1c7ca5fcba84c4ca270f5ba',
+          title: 'Lomba Pendukung : Kids Warrior',
+          file: process.env.VUE_APP_BASE_URL + 'event/logo/kw.pdf',
+          desc: 'Lomba Kids Warrior oleh peserta Lomba Pendukung'
+        },
+        {
+          img: 'https://img.freepik.com/free-photo/boy-playing-with-plastic-blocks-library-room-school_1249-516.jpg?w=1060&t=st=1679994562~exp=1679995162~hmac=7f7545eae04d848c4ead459b90a3c51f6f33639b7b213309c39b8365732563eb',
+          title: 'Lomba Pendukung : Lego Race',
+          file: process.env.VUE_APP_BASE_URL + 'event/logo/lr.pdf',
+          desc: 'Lomba Lego Race oleh peserta Lomba Pendukung'
+        },
+        {
+          img: 'https://img.freepik.com/free-photo/painting-dark-skinned-primary-school-age-boy-drawing-with-blue-pencil-sideways-camera-while-sitting-table-room-daylight_259150-59521.jpg?w=1060&t=st=1679993347~exp=1679993947~hmac=912c8d792381a178667c595025ae2c679deb3b2cc0ffa24397fc28bd51b62de1',
+          title: 'Lomba Pendukung Jenjang SD : Coloring',
+          file: process.env.VUE_APP_BASE_URL + 'event/logo/crsd.pdf',
+          desc: 'Lomba mewarnai oleh peserta Lomba Pendukung'
+        },
       ],
     }
   }
